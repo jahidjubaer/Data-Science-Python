0353dd3c18c56a9e02b03102f47be9fdd5f53543
Jahid Hasan <jahidjubaer17@gmail.com>
2024-10-31 23:35:25 +0600

    basic python solved added

--- HakerRank/IfElse.py
+++ HakerRank/IfElse.py
@@ -1,2 +1,12 @@
 n = int(input())
-print
+
+if n % 2 == 1 :
+    print("Weird\n")
+elif n % 2 == 0 :
+    if 2 <= n <= 5 :
+        print("Not Weird\n")
+    elif 6 <= n <= 20 :
+        print("Weird\n")
+    elif n > 20 :
+        print("Not Weird\n")
+
---
+++ HakerRank/PrintFunction.py
@@ -0,0 +1,8 @@
+def printf(n) :
+    ch = 
+    for i in range(1, n, 1):
+        ch[i] = i
+   print(ch)
+
+n = int(input())
+printf(n)
